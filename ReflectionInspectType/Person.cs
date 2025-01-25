a1e215a43fff6748accfa08b6523ff9e359bdfa6
pmgradinmitev <pmg.tadinmitev92@gmail.com>
2025-01-25 22:01:45 +0200

    Add project files.

---
+++ ReflectionInspectType/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionInspectType
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Name: {Name}");
+        }
+    }
+}
---
+++ ReflectionInspectType/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+namespace ReflectionInspectType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(Person);
+
+            Console.WriteLine($"Class Name: {type.Name}");
+
+            // Get properties
+            Console.WriteLine("Properties:");
+            foreach (var property in type.GetProperties())
+            {
+                Console.WriteLine($"- {property.Name} ({property.PropertyType.Name})");
+            }
+
+            // Get methods
+            Console.WriteLine("Methods:");
+            foreach (var method in type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly))
+            {
+                Console.WriteLine($"- {method.Name}");
+            }
+        }
+    }
+}
